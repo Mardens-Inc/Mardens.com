bc1526356a3ebeac6b8d0e0c9b33af31f16ffdf9
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-10-21 14:27:23 -0400

    Adjusted `Hero` component layout: added left margin to container and increased paragraph width for improved alignment and readability.

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -4,9 +4,9 @@ export function Hero()
 {
     return (
             <div className={"flex flex-col items-start justify-center h-[500px] bg-cover bg-center bg-no-repeat md:px-16"} style={{backgroundImage: `url("${BannerImage}")`}}>
-                <div className={"w-4/5 md:w-2/3 xl:w-3/5 max-w-[1600px]"}>
+                <div className={"w-4/5 md:w-2/3 xl:w-3/5 max-w-[1600px] ml-20"}>
                     <h1 className={"font-black font-headers text-primary uppercase leading-none text-7xl"}>Maine's Best Bargain Store</h1>
-                    <p className={"mt-4 text-black w-1/2"}>Purveyors of insurance salvages, closeouts, bankruptcies, liquidations, and good old fashioned deals.</p>
+                    <p className={"mt-4 text-black w-3/5"}>Purveyors of insurance salvages, closeouts, bankruptcies, liquidations, and good old fashioned deals.</p>
                 </div>
             </div>
     );
