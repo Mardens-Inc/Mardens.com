300a88b53319502af7798e2029f1a0efeb2b4ffa
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-10-21 15:04:21 -0400

    Integrated `BenefitsSection` and `ProductCategories` components into `Home` page to enhance promotional content and category discoverability.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,14 @@
 import {Hero} from "../components/home/Hero.tsx";
+import {BenefitsSection} from "../components/home/BenefitsSection.tsx";
+import {ProductCategories} from "../components/home/ProductCategories.tsx";
 
 export default function Home()
 {
     return (
         <div className={"flex flex-col"}>
             <Hero/>
+            <BenefitsSection/>
+            <ProductCategories/>
         </div>
     );
 }
