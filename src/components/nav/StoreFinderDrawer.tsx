803342ae176a886aa24679ecc4fb0cc920811eac
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-10-22 14:10:48 -0400

    Added `StoreFinderDrawer` search feature with store listings, location input, and open/close status.

--- src/components/nav/StoreFinderDrawer.tsx
+++ src/components/nav/StoreFinderDrawer.tsx
@@ -1,11 +1,126 @@
 import {Icon} from "@iconify-icon/react";
-import {Button} from "@heroui/react";
+import {Button, Input} from "@heroui/react";
 import {motion} from "framer-motion";
 
 type StoreFinderDrawerProps = {
     open: boolean;
     onClose: () => void;
 }
+
+const storesToShow: StoreLocationItemProps[] = [
+    {
+        name: "Waterville",
+        address: "458 Kennedy Memorial Dr, Waterville, ME 04901",
+        schedule: {
+            open: 9, // 9AM
+            close: 20 // 8PM
+        }
+    },
+    {
+        name: "Farmington",
+        address: "634 Wilton Road, Farmington, ME",
+        schedule: {
+            open: 9,
+            close: 20
+        }
+    },
+    {
+        name: "Brewer",
+        address: "564 Wilson St, Brewer, ME 04412",
+        schedule: {
+            open: 9,
+            close: 20
+        }
+    },
+    {
+        name: "Lewiston",
+        address: "750 Main St, Lewiston, ME 04240",
+        schedule: {
+            open: 9,
+            close: 20
+        }
+    },
+    {
+        name: "Gray",
+        address: "65 W Gray Rd, Gray, ME 04039",
+        schedule: {
+            open: 9,
+            close: 18
+        }
+    },
+    {
+        name: "Ellsworth",
+        address: "33 Mardens Way Suite A, Ellsworth, ME 04605",
+        schedule: {
+            open: 9,
+            close: 20
+        }
+    },
+    {
+        name: "Scarborough",
+        address: "451 Payne Rd, Scarborough, ME 04074",
+        schedule: {
+            open: 9,
+            close: 19
+        }
+    },
+    {
+        name: "Lincoln",
+        address: "28 Main St, Lincoln, ME 04457",
+        schedule: {
+            open: 9,
+            close: 18
+        }
+    },
+    {
+        name: "Biddeford",
+        address: "435 Elm St, Biddeford, ME 04005",
+        schedule: {
+            open: 9,
+            close: 20
+        }
+    },
+    {
+        name: "Sanford",
+        address: "1247 Main Street, Sanford, ME",
+        schedule: {
+            open: 9,
+            close: 20
+        }
+    },
+    {
+        name: "Calais",
+        address: "189 Main St, Calais, ME 04619",
+        schedule: {
+            open: 9,
+            close: 18
+        }
+    },
+    {
+        name: "Houlton",
+        address: "120 North St, Houlton, ME 04730",
+        schedule: {
+            open: 9,
+            close: 20
+        }
+    },
+    {
+        name: "Presque Isle",
+        address: "803 Main St, Presque Isle, ME 04769",
+        schedule: {
+            open: 9,
+            close: 20
+        }
+    },
+    {
+        name: "Madawaska",
+        address: "81 Fox St, Madawaska, ME 04756",
+        schedule: {
+            open: 9,
+            close: 20
+        }
+    }
+];
 
 export function StoreFinderDrawer(props: StoreFinderDrawerProps)
 {
@@ -20,16 +135,80 @@ export function StoreFinderDrawer(props: StoreFinderDrawerProps)
                 data-open={open}
             />
             <motion.div
-                className={"pt-24 w-[450px] max-w-[95vw] overflow-y-auto h-full ml-auto p-4 bg-white shadow-2xl flex flex-col gap-2 items-center text-center z-10"}
+                className={"pt-24 w-[450px] px-8 max-w-[95vw] overflow-y-auto h-full ml-auto p-4 bg-white shadow-2xl flex flex-col gap-2 items-center text-center z-10"}
                 initial={{opacity: 0, x: 200}}
                 animate={{opacity: open ? 1 : 0, x: open ? 0 : 200}}
             >
-                <Button isIconOnly radius={"full"} variant={"ghost"} className={"absolute right-4"} onPress={onClose}><Icon icon={"mingcute:close-line"} /></Button>
+                <Button isIconOnly radius={"full"} variant={"ghost"} className={"absolute right-4"} onPress={onClose}><Icon icon={"mingcute:close-line"}/></Button>
                 <h2 className={"text-primary font-bold text-3xl uppercase"}>Select a Store</h2>
                 <div className={"flex items-center underline text-black text-base relative hover:text-primary focus:text-primary transition"}><Icon icon={"tdesign:location"}/> use my current location?</div>
                 <p className={"text-xs text-center opacity-75 my-3 mx-auto max-w-xs"}>Each store is different—think different merchandise and different deals. Pick a favorite or stop by a few and find smart ways to save more.</p>
+
+                <div className={"flex flex-row gap-1 w-full"}>
+                    <Input
+                        placeholder={"Enter your ZIP or city"}
+                        radius={"none"}
+                        size={"lg"}
+                        variant={"bordered"}
+                    />
+                    <Button radius={"none"} size={"lg"} className={"uppercase font-text"} color={"primary"}>find us</Button>
+                </div>
+
+                <div className={"flex flex-col gap-2 w-full overflow-auto"}>
+                    {storesToShow.map((store, index) => (
+                        <StoreLocationItem key={index} {...store}/>
+                    ))}
+                </div>
+
             </motion.div>
 
+        </div>
+    );
+}
+
+type StoreLocationItemProps = {
+    name: string;
+    address: string;
+    schedule: {
+        // The time that the store opens (1 is 1am and 13 is 1pm)
+        // 0 is 12am and 23 is 11pm.
+        open: number,
+        // The time that the store closes (1 is 1am and 13 is 1pm)
+        // 0 is 12am and 23 is 11pm.
+        close: number
+    }
+}
+
+function StoreLocationItem(props: StoreLocationItemProps)
+{
+    const {name, address, schedule} = props;
+
+    const now = new Date();
+    const currentHour = now.getHours();
+    const isOpen = currentHour >= schedule.open && currentHour < schedule.close;
+
+    const formatTime = (hour: number) =>
+    {
+        const ampm = hour >= 12 ? "pm" : "am";
+        const formattedHour = hour % 12 || 12;
+        return `${formattedHour}${ampm}`;
+    };
+
+    const statusText = isOpen
+        ? `Open today until ${formatTime(schedule.close)}`
+        : `Closed • Opens ${formatTime(schedule.open)}`;
+
+    return (
+        <div className={"flex flex-col gap-2 w-full border-b border-black/10 py-4 font-text transition-none"}>
+            <div className={"flex flex-row gap-2"}>
+                <p className={"font-accent bg-[#f7f7f4] rotate-[-5deg] text-2xl py-2 px-4"}>{name}</p>
+                <div className={"flex flex-row items-center ml-auto font-text"}>
+                    <Button variant={"light"} radius={"none"} className={"text-xs uppercase"} startContent={<Icon icon={"fluent:list-24-filled"} width={16}/>}>Details</Button>
+                    <Button variant={"light"} radius={"none"} className={"text-xs uppercase"} startContent={<Icon icon={"material-symbols:distance-outline-rounded"} width={16}/>}>14.4 mi</Button>
+                </div>
+            </div>
+            <p className={"text-xs text-start"}>{address}</p>
+            <p className={"text-xs text-start data-[open=true]:text-[#32993e] text-danger"} data-open={isOpen}>{statusText}</p>
         </div>
     );
 }
