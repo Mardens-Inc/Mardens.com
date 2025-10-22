38e5e3b6c5665b7a1601d4c39f36e484ccc05153
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-10-22 10:09:47 -0400

    Integrated `InstagramFeed` and `Footer` components into `Home` page layout.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ import {Hero} from "../components/home/Hero.tsx";
 import {BenefitsSection} from "../components/home/BenefitsSection.tsx";
 import {ProductCategories} from "../components/home/ProductCategories.tsx";
 import {LocationsShortList} from "../components/home/LocationsShortList.tsx";
+import {InstagramFeed} from "../components/InstagramFeed.tsx";
+import {Footer} from "../components/Footer.tsx";
 
 export default function Home()
 {
@@ -10,10 +12,11 @@ export default function Home()
             <Hero/>
             <BenefitsSection/>
             <div className={"bg-[#f7f7f4]"}>
-
-            <ProductCategories/>
-            <LocationsShortList />
+                <ProductCategories/>
+                <LocationsShortList/>
             </div>
+            <InstagramFeed/>
+            <Footer/>
         </div>
     );
 }
